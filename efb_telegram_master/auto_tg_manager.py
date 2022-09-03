638c9a2d02d427ffd0c2d1ab4f8ed941d477a4d8
mlch911 <mlch911@mluoc.tech>
2022-09-03 13:43:57 +0800

    Fix Bug

--- efb_telegram_master/auto_tg_manager.py
+++ efb_telegram_master/auto_tg_manager.py
@@ -44,7 +44,7 @@ class AutoTGManager(LocaleMixin):
                                              workdir=ehforwarderbot.utils.get_data_path(channel.channel_id))
             self.tg_loop = asyncio.new_event_loop()
             self.tg_loop.run_until_complete(self._start_tg_client_if_needed())
-            self.tg_loop.run_until_complete(self._stop_tg_client())
+            # self.tg_loop.run_until_complete(self._stop_tg_client())
 
     def create_tg_group_if_needed(self, chat: ETMChatType) -> Optional[utils.EFBChannelChatIDStr]:
         if not self.tg_client:
@@ -131,7 +131,7 @@ class AutoTGManager(LocaleMixin):
             await self._add_tg_group_to_folder_if_needed(chat, tg_chat)
             await self._archive_tg_chat_if_needed(chat, tg_chat)
             await self._mute_tg_group_if_needed(chat, tg_chat)
-            await self._stop_tg_client()
+            # await self._stop_tg_client()
             return tg_chat
         except Exception:
             self.logger.exception("Unknown error caught when creating TG group.")
